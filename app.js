98e0099241f007254cd4ef4ddb9d7a8a99aaf101
raeinking <60474712+raeinking@users.noreply.github.com>
2021-03-21 19:07:20 -0700

    all change

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const goal = document.querySelector(".goal");
 const nope = document.querySelector(".nope");
 
 btn.addEventListener("click", function(){
-    document.getElementById("nope").innerHTML = goal.value
+    document.getElementById("nope").innerHTML = goal.value;
+
 });
 
 nope.addEventListener("click", function(){
     document.getElementById("nope").innerHTML = ""
 });
-
 
